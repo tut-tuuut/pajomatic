6fa33c43fcfce42b0621e679323156a3e25908e5
Agnès Haasser <ahaasser@tea-ebook.com>
2015-01-28 22:03:24 +0100

    Mod: include model, view and controller.js

---
+++ js/model.js
@@ -0,0 +1,11 @@
+var pajomatic_model = (function (undefined) {
+    var exports;
+
+    exports = {};
+
+    exports.plop = function () {
+        return 'hell yeah';
+    }
+
+    return exports;
+})();
