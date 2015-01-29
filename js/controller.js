2ceb8a5740082be4b2f5332f32edac5249727891
Agnès Haasser <ahaasser@tea-ebook.com>
2015-01-29 13:59:59 +0100

    Basic salary and indemnity calculation

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     // observe changes on input form
     $('#incomplete_year_input').on('change', 'input', function (event) {
-        pajomatic_view.extractFormData('incomplete_year_input');
+        var input = pajomatic_view.extractFormData('incomplete_year_input');
+        pajomatic_model.calculatePajemploiDeclaration(input);
     });
     // extract data from view
     // pass data to the model
--- js/model.js
+++ js/model.js
@@ -3,9 +3,36 @@ var pajomatic_model = (function (undefined) {
 
     exports = {};
 
-    exports.plop = function () {
-        return 'hell yeah';
-    }
+    exports.calculatePajemploiDeclaration = function (input) {
+        console.log(input);
+
+        var nb_heures_normales_mensualise = input.nb_semaines * input.nb_heures_normales / 12;
+        var nb_heures_majorees_mensualise = input.nb_semaines * input.nb_heures_majorees / 12;
+        var nb_heures_majorees = Math.round(nb_heures_majorees_mensualise + input.nb_supp_majorees);
+
+        var nb_jours_mensualise = Math.ceil(input.nb_semaines * input.nb_jours_par_semaine / 12);
+
+        var nb_jours_activite = nb_jours_mensualise - input.absences_nb_jours;
+
+        var salaire_mensuel_h_normales = nb_heures_normales_mensualise * input.salaire_net_normal;
+        var salaire_mensuel_h_majorees = nb_heures_majorees_mensualise * input.salaire_net_normal * (100 + input.majoration_heures_majorees) / 100;
+        var salaire_mensualise = (salaire_mensuel_h_majorees + salaire_mensuel_h_normales);
+        var salaire_net_total = Math.round((salaire_mensualise + input.montant_conges_payes)*100)/100;
+
+        var output = {
+            nb_heures_normales : Math.round(nb_heures_normales_mensualise),
+            nb_jours_activite : nb_jours_activite,
+            nb_jours_conges_payes : Math.ceil(input.nb_jours_conges_payes),
+            salaire_horaire_net_normal : input.salaire_net_normal,
+            nb_heures_majorees : nb_heures_majorees,
+            nb_heures_complementaires : Math.round(input.nb_supp_complementaires),
+            salaire_net_total : salaire_net_total,
+            indemnites_entretien : 0.00,
+            indemnites_repas : 0.00
+        };
+        console.log(output);
+        return output;
+    };
 
     return exports;
 })();
