f84ca820b23f4e930e91edc0ba7d43bcd8dfd378
Agnès H <agnes.haasser@gmail.com>
2015-01-31 11:21:27 +0100

    Add: calculation of indemnités + montant total

--- js/model.js
+++ js/model.js
@@ -17,6 +17,10 @@ var pajomatic_model = (function (undefined) {
         var salaire_mensualise = (salaire_mensuel_h_majorees + salaire_mensuel_h_normales);
         var salaire_net_total = Math.round((salaire_mensualise + input.montant_conges_payes)*100)/100;
 
+        var nb_jours_reel = input.nb_jours_accueil_reel;
+        var indemnites_entretien = input.indemnite_entretien * nb_jours_reel;
+        var indemnites_repas = (input.indemnite_repas + input.indemnite_gouter) * nb_jours_reel;
+
         var output = {
             nb_heures_normales : Math.round(nb_heures_normales_mensualise),
             nb_jours_activite : nb_jours_activite,
@@ -25,8 +29,9 @@ var pajomatic_model = (function (undefined) {
             nb_heures_majorees : nb_heures_majorees,
             nb_heures_complementaires : Math.round(input.nb_supp_complementaires),
             salaire_net_total : salaire_net_total,
-            indemnites_entretien : 0.00,
-            indemnites_repas : 0.00
+            indemnites_entretien : indemnites_entretien,
+            indemnites_repas : indemnites_repas,
+            total_a_payer : salaire_net_total + indemnites_repas + indemnites_entretien
         };
         return output;
     };
