f5163d25bfad522d8fff5e143475952f85c98b5a
Agnès H <agnes.haasser@gmail.com>
2015-04-19 20:43:25 +0200

    Mod: understanding deepEqual

--- js/unit/tests.js
+++ js/unit/tests.js
@@ -1,3 +1,6 @@
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
+  var tata = 'x';
+  var toto = 'z';
+  assert.deepEqual({toto: tata, titi: toto}, {titi: toto, toto:tata}, "Comparaison d'objets");
 });
